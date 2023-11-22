Shringe/dwl

commit c869f7f56fda4dd2699f3d261f65948fbdf6bdf9

resize clients on commit
Fixes: https://github.com/djpohly/dwl/issues/515
This reverts commit 985417b648d1d812fc7e4bf00e5981981c2612b1.

dwl.c
@@ -738,6 +738,9 @@ commitnotify(struct wl_listener *listener, void *data)
 {
 	Client *c = wl_container_of(listener, c, commit);
 
+	if (client_surface(c)->mapped)
+		resize(c, c->geom, (c->isfloating && !c->isfullscreen));
+
 	/* mark a pending resize as completed */
 	if (c->resize && c->resize <= c->surface.xdg->current.configure_serial)
 		c->resize = 0;

commit 908d1baa09b6715348c84c76d9205df9920282c6

Do not send repeated xdg_toplevel.configure_bounds
Fixes: c869f7f56fda4dd2699f3d261f65948fbdf6bdf9

client.h
@@ -112,8 +112,12 @@ client_set_bounds(Client *c, int32_t width, int32_t height)
 		return 0;
 #endif
 	if (wl_resource_get_version(c->surface.xdg->toplevel->resource) >=
-			XDG_TOPLEVEL_CONFIGURE_BOUNDS_SINCE_VERSION && width >= 0 && height >= 0)
+			XDG_TOPLEVEL_CONFIGURE_BOUNDS_SINCE_VERSION && width >= 0 && height >= 0
+			&& (c->bounds.width != width || c->bounds.height != height)) {
+		c->bounds.width = width;
+		c->bounds.height = height;
 		return wlr_xdg_toplevel_set_bounds(c->surface.xdg->toplevel, width, height);
+	}
 	return 0;
 }
 

dwl.c
@@ -118,6 +118,7 @@ typedef struct {
 	struct wl_listener set_title;
 	struct wl_listener fullscreen;
 	struct wlr_box prev; /* layout-relative, includes border */
+	struct wlr_box bounds;
 #ifdef XWAYLAND
 	struct wl_listener activate;
 	struct wl_listener associate;