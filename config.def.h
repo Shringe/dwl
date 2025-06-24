773d8ff3ee9d108c796e53fc1985cc6478669e62
Shringe <git@nsaria.com>
2025-06-23 19:30:51 -0500

    fixed keybind

--- config.def.h
+++ config.def.h
@@ -153,7 +153,7 @@ static const Key keys[] = {
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
 	{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
 	{ MODKEY,                    XKB_KEY_F5,        togglefullscreenadaptivesync, {0} },
-  { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_g,         togglefullscreenkeyinhibit, {0} },
+  { MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_G,         togglefullscreenkeyinhibit, {0} },
 	{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
 	{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
